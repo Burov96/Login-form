b7b370ec348b547d8f6d75a984b04784e15155d2
Your Name <your_email@example.com>
2024-11-24 22:50:52 +0200

    deploy to github pages

---
+++ src/components/Error.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { DotLottiePlayer } from '@dotlottie/react-player';
+
+interface ErrorProps {
+  width?: number;
+  height?: number;
+}
+
+const ErrorLottie = ({ width = 200, height = 200 }: ErrorProps) => {
+  return (
+      <DotLottiePlayer
+        src="/failure.lottie"
+        autoplay
+        loop
+        style={{ width: `${width}px`, height: `${height}px` }}
+      />
+  );
+};
+
+export {ErrorLottie};
